9861fc001b025cb81c0d53bacd962934a3660e6c
Tomas <tomibudeguer20@gmail.com>
2022-10-11 17:01:27 -0300

    agregamos de cometario los tipos de peticiones

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,15 +1,21 @@
 // archivo que nos sirve para hacer las consultas a la api (json-server)
 
-const URL = 'http://localhost:3004/productos';
+const URL = "http://localhost:3004/productos";
 
-// peticion GET que trae todos los productos
-export const consultarAPI = async()=>{
-    try {
-        const respuesta = await fetch(URL)
-        const listaProductos = await respuesta.json()
-        // console.log(listaProductos)
-        return listaProductos
-    } catch (error) {
-        console.log(error)
-    }
-}
+// tipos de peticiones
+// peticion GET, que trae todos los productos o un producto
+// peticion POST, para crear un producto, login
+// peticion DELETE, para borrar un porducto
+// peticion PUT, para modificar un producto
+
+
+export const consultarAPI = async () => {
+  try {
+    const respuesta = await fetch(URL);
+    const listaProductos = await respuesta.json();
+    // console.log(listaProductos)
+    return listaProductos;
+  } catch (error) {
+    console.log(error);
+  }
+};
